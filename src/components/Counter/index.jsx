4ddcfbe2284c6887432deb7bd89237aa8381ecf3
Marta Prystatska <tradlisboa@gmail.com>
2024-06-23 22:25:16 +0100

    add autoClickInterval: Armazena o ID do intervalo para controle do autoClick. autoClickTimeLeft: Conta regressivamente o tempo restante para o autoClick.

--- src/components/Counter/index.jsx
+++ src/components/Counter/index.jsx
@@ -2,30 +2,48 @@ import React, { Component } from "react";
 import styles from "./Counter.module.css";
 
 class Counter extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       count: 0,
       isAdd: true,
+      autoClickInterval: null,
+      autoClickTimeLeft: 30, // Tempo em segundos
     };
   }
-    handelChangeMode = () => {
-        const { isAdd } = this.state;
-        this.setState({ isAdd: !isAdd })
-    }
-    handelCount = () => {
-        this.setState((state, props) => {
-            const { count, isAdd } = state;
-            const newCount = isAdd ? count + props.step : count - props.step;
-            return { count: newCount };
-        })
+
+
+  autoClick = () => {
+    const { count, isAdd, autoClickTimeLeft } = this.state;
+    if (autoClickTimeLeft <= 0) {
+      this.stopAutoClick();
+      return;
     }
 
-  render() {
+    const newCount = isAdd ? count + this.props.step : count - this.props.step;
+    this.setState((prevState) => ({
+      count: newCount,
+      autoClickTimeLeft: prevState.autoClickTimeLeft - 1,
+    }));
+  };
+
+  handelChangeMode = () => {
+    const { isAdd } = this.state;
+    this.setState({ isAdd: !isAdd });
+  };
+
+  handelCount = () => {
     const { count, isAdd } = this.state;
+    const newCount = isAdd ? count + this.props.step : count - this.props.step;
+    this.setState({ count: newCount });
+  };
+
+  render() {
+    const { count, isAdd, autoClickTimeLeft } = this.state;
     return (
       <article className={styles.counter}>
         <h2>{count}</h2>
+        <p>AutoClick Time Left: {autoClickTimeLeft}s</p>
         <button onClick={this.handelCount}>{isAdd ? "add" : "sub"}</button>
         <button onClick={this.handelChangeMode}>change</button>
       </article>
