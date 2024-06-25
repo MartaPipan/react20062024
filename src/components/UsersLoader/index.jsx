d6a34e535288832aee98fd331fa78f4d48440dc0
Marta Prystatska <tradlisboa@gmail.com>
2024-06-25 09:58:12 +0100

    add componentDidMount and componentDidUpdate

--- src/components/UsersLoader/index.jsx
+++ src/components/UsersLoader/index.jsx
@@ -8,28 +8,52 @@ class UserLoader extends Component {
       users: [],
       isPending: false,
       error: null,
+      currentPage: 1,
     };
   }
-    componentDidMount() {
-        this.setState({ isPending: true });
-    fetch("https://randomuser.me/api/?results=5&page1=1&seed=fpm2024-1")
+    load = () => {
+            const { currentPage } = this.state;
+    this.setState({ isPending: true });
+    fetch(`https://randomuser.me/api/?results=5&page=${currentPage}&seed=fpm2024-1`)
       .then((response) => response.json())
-        .then((data) => {
-            if (data.error){
-              throw new Error(data.error)
-          }
+      .then((data) => {
+        if (data.error) {
+          throw new Error(data.error);
+        }
         this.setState({ users: data.results });
       })
       .catch((error) => {
         this.setState({ error: error });
       })
-        .finally(() => {
-            this.setState({ isPending: false });
+      .finally(() => {
+        this.setState({ isPending: false });
       });
   }
+    componentDidMount() {
+        this.load()
+    }
+  componentDidUpdate(prevProps, prevState) {
+    if(prevState.currentPage !== this.state.currentPage){
+      this.load();
+    }
+  }
   showUsers = (user) => <li key={user.login.uuid}>{user.name.first}</li>;
+  prevPage = () => {
+    this.setState((state, props) => {
+      const { currentPage } = state;
+      if (currentPage > 1) {
+        return { currentPage: currentPage - 1 };
+      }
+    });
+  };
+  nextPage = () => {
+    this.setState((state) => {
+      const { currentPage } = state;
+      return { currentPage: currentPage + 1 };
+    });
+  };
   render() {
-    const { users, isPending, error } = this.state;
+    const { users, isPending, error, currentPage } = this.state;
     if (isPending) {
       return <div>Loading...</div>;
     }
@@ -38,12 +62,12 @@ class UserLoader extends Component {
     }
     return (
       <section>
-            <h2>Users:</h2>
-            <div>
-                <button>&lt;</button> 
-                <span> 1 </span>
-                <button>&gt;</button>
-            </div>
+        <h2>Users:</h2>
+        <div>
+          <button onClick={this.prevPage}>&lt;</button>
+          <span> {currentPage} </span>
+          <button onClick={this.nextPage}>&gt;</button>
+        </div>
         {users.length ? (
           <ul>{users.map(this.showUsers)}</ul>
         ) : (
