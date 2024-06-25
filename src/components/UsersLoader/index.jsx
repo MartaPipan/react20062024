23689fbd252c0e6d65c4f6ebfe260d06641251b3
Marta Prystatska <tradlisboa@gmail.com>
2024-06-25 08:43:11 +0100

    Add fetch random in  UserLoader

--- src/components/UsersLoader/index.jsx
+++ src/components/UsersLoader/index.jsx
@@ -1,41 +1,51 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 class UserLoader extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            users: [],
-            isPending: false,
-            error: null,
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [],
+      isPending: false,
+      error: null,
+    };
+  }
+    componentDidMount() {
+        this.setState({ isPending: true });
+    fetch("https://randomuser.me/api/?results=5")
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({ users: data.results });
+      })
+      .catch((error) => {
+        this.setState({ error: error });
+      })
+        .finally(() => {
+            this.setState({ isPending: false });
+      });
+  }
+  showUsers = (user) => <li key={user.login.uuid}>{user.name.first}</li>;
+  render() {
+    const { users, isPending, error } = this.state;
+    if (isPending) {
+      return <div>Loading...</div>;
     }
-    showUsers = (user) => <li key={user.login.uuid}>{user.name.first}</li>
-    render() {
-        const { users, isPending, error } = this.state;
-        if (isPending) {
-            return <div>Loading...</div>
-        }
-        if (error) {
-            return <div>ERROR!!!</div>
-        }
-        return (<section>
-            <h2>Users:</h2>
-            {users.length ? <ul>
-                {
-                    users.map(this.showUsers)
-                }
-            </ul> : <p>empty users</p>}
-        </section>
-        );
+    if (error) {
+      return <div>ERROR!!!</div>;
     }
+    return (
+      <section>
+        <h2>Users:</h2>
+        {users.length ? (
+          <ul>{users.map(this.showUsers)}</ul>
+        ) : (
+          <p>empty users</p>
+        )}
+      </section>
+    );
+  }
 }
 
-
-UserLoader.propTypes = {
-
-};
-
+UserLoader.propTypes = {};
 
 export default UserLoader;
