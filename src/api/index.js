7bfe24f9720a4531cf840ecd0fc93bb747782a86
Marta Prystatska <tradlisboa@gmail.com>
2024-06-27 07:29:42 +0100

    optimization: getUsers in api

---
+++ src/api/index.js
@@ -0,0 +1,4 @@
+export const getUsers = (currentPage) => {
+    return fetch(`https://randomuser.me/api/?results=5&page=${currentPage}&seed=fpm2024-1`)
+        .then((response) => response.json())
+};
