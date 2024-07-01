2d10b07940e44a8db9fd4b421e0f1e3e76efd303
Marta Prystatska <tradlisboa@gmail.com>
2024-07-01 07:56:03 +0100

    add ErrorPage (with not clear efect=need correct use from didMount)

--- src/App.js
+++ src/App.js
@@ -4,16 +4,20 @@ import WindowWork from "./components/WindowWork";
 import UsersLoader from "./components/UsersLoader";
 import CounterSection from "./components/CounterSection/index";
 import Header from "./components/Header";
+
+import ErrorPage from "./components/ErrorPage";
 function App() {
   return (
     <>
       <BrowserRouter>
         <Header/>
         <main>
-        <Routes>
-          <Route path="/" element={<WindowWork />}></Route>
-          <Route path="/users" element={<UsersLoader />}></Route>
-          <Route path="/counter" element={<CounterSection />}></Route>
+          <Routes>
+            <Route path="/" element={<WindowWork />}></Route>
+            <Route path="/users" element={<UsersLoader />}></Route>
+            <Route path="/counter" element={<CounterSection />}></Route>
+
+            <Route path="*" element={<ErrorPage />}></Route>
           </Routes>
         </main>
         <footer>2024</footer>
