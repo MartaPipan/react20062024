5fb4d24883a629bfcfbed912c4666bd4d832b05a
Marta Prystatska <tradlisboa@gmail.com>
2024-07-02 20:00:01 +0100

    start use CONTEXT (PROVIDER+CONSUMER)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header";
@@ -12,18 +11,18 @@ import ErrorPage from "./pages/ErrorPage";
 import LoaderPage from "./pages/LoaderPage";
 import EventsBlock from "./pages/LoaderPage/EventsBlock";
 import PhonesBlock from "./pages/LoaderPage/PhonesBlock";
+import { UserContext } from "./contexts";
 
-
-class App extends Component{
+class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       user: {
-        id:4,
-        name: 'Brad',
-        email: 'brad@gmail.com',
+        id: 4,
+        name: "Brad",
+        email: "brad@gmail.com",
         password: 123,
-        ava: '/images/noname.png',
+        ava: "/images/noname.png",
       },
     };
   }
@@ -31,24 +30,26 @@ class App extends Component{
     const { user } = this.state;
     return (
       <>
-        <BrowserRouter>
-          <Header user={user} />
-          <main>
-            <Routes>
-              <Route path="/" element={<HomePage user={ user} />}></Route>
-              <Route path="/users" element={<UsersLoaderPage />}></Route>
-              <Route path="/counter" element={<CounterPage />}></Route>
+        <UserContext.Provider value={user}>
+          <BrowserRouter>
+            <Header />
+            <main>
+              <Routes>
+                <Route path="/" element={<HomePage />}></Route>
+                <Route path="/users" element={<UsersLoaderPage />}></Route>
+                <Route path="/counter" element={<CounterPage />}></Route>
 
-              <Route path="/load/" element={<LoaderPage />}>
-                <Route path="events" element={<EventsBlock />}></Route>
-                <Route path="phones" element={<PhonesBlock />}></Route>
-              </Route>
+                <Route path="/load/" element={<LoaderPage />}>
+                  <Route path="events" element={<EventsBlock />}></Route>
+                  <Route path="phones" element={<PhonesBlock />}></Route>
+                </Route>
 
-              <Route path="*" element={<ErrorPage />}></Route>
-            </Routes>
-          </main>
-          <footer>2024</footer>
-        </BrowserRouter>
+                <Route path="*" element={<ErrorPage />}></Route>
+              </Routes>
+            </main>
+            <footer>2024</footer>
+          </BrowserRouter>
+        </UserContext.Provider>
       </>
     );
   }
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,13 +1,20 @@
-import React from 'react';
-import Menu from "../Menu"
+import React from "react";
+import Menu from "../Menu";
+import { UserContext } from "../../contexts/index";
 
-const Header = ({user:{ava}}) => {
-    return (
-        <header>
+const Header = () => {
+  return (
+    <UserContext.Consumer>
+      {({ava}) => {
+        return (
+          <header>
             <Menu />
-            <img src={ava}  alt='ava' width={50}/>
-        </header>
-    );
-}
+            <img src={ava} alt="ava" width={50} />
+          </header>
+        );
+      }}
+    </UserContext.Consumer>
+  );
+};
 
 export default Header;
