cde65592392fa29cff9b5acdb948d9e68ae90a63
Marta Prystatska <tradlisboa@gmail.com>
2024-07-02 11:12:23 +0100

    2-07-2024 Start.Context. (Step1 change App function->class component))

--- src/App.js
+++ src/App.js
@@ -1,43 +1,41 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./App.css"
+import "./App.css";
 import Header from "./components/Header";
 import HomePage from "./pages/HomePage";
-import UsersLoaderPage from './pages/UsersLoaderPage';
+import UsersLoaderPage from "./pages/UsersLoaderPage";
 import CounterPage from "./pages/CounterPage";
 
-import ErrorPage from "./pages/ErrorPage"
+import ErrorPage from "./pages/ErrorPage";
 
 import LoaderPage from "./pages/LoaderPage";
 import EventsBlock from "./pages/LoaderPage/EventsBlock";
-import PhonesBlock from './pages/LoaderPage/PhonesBlock';
+import PhonesBlock from "./pages/LoaderPage/PhonesBlock";
 
+class App {
+  render() {
+    return (
+      <>
+        <BrowserRouter>
+          <Header />
+          <main>
+            <Routes>
+              <Route path="/" element={<HomePage />}></Route>
+              <Route path="/users" element={<UsersLoaderPage />}></Route>
+              <Route path="/counter" element={<CounterPage />}></Route>
 
+              <Route path="/load/" element={<LoaderPage />}>
+                <Route path="events" element={<EventsBlock />}></Route>
+                <Route path="phones" element={<PhonesBlock />}></Route>
+              </Route>
 
-function App() {
-  return (
-    <>
-      <BrowserRouter>
-        <Header />
-        <main>
-          <Routes>
-            <Route path="/" element={<HomePage/>}></Route>
-            <Route path="/users" element={<UsersLoaderPage />}></Route>
-            <Route path="/counter" element={<CounterPage />}></Route>
-
-            <Route path="/load/" element={<LoaderPage />}>
-              <Route path="events" element={<EventsBlock />}></Route>
-              <Route path="phones" element={<PhonesBlock/>}></Route>
-            </Route>
-
-            <Route path="*" element={<ErrorPage />}></Route>
-          </Routes>
-        </main>
-        <footer>2024</footer>
-      </BrowserRouter>
-    </>
-  );
+              <Route path="*" element={<ErrorPage />}></Route>
+            </Routes>
+          </main>
+          <footer>2024</footer>
+        </BrowserRouter>
+      </>
+    );
+  }
 }
 
 export default App;
-
-
