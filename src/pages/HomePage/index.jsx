675397d7d816d06372d6eaf3cdffbceea4e78811
Marta Prystatska <tradlisboa@gmail.com>
2024-07-04 00:09:49 +0100

    add styles for HomePage with inline metod (for styles in HomePageIndex,)

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,13 +1,28 @@
 import React from "react";
+import { ThemeContext } from "../../contexts";
 import WindowWork from "../../components/WindowWork";
 import Tree from "../../components/Tree";
+import CONSTANTS from '../../constants';
+const { THEME } = CONSTANTS;
 
 const HomePage = (user) => {
   return (
-    <div>
-      <WindowWork />
-      <Tree user={user} />
-    </div>
+    <>
+      <ThemeContext.Consumer>
+        {([theme]) => {
+            const styles = {
+              backgroundColor: theme === THEME.LIGHT?'white':'black',
+              color: theme === THEME.LIGHT?'black':'white',
+            }
+            return (
+              <div style={styles}>
+                <WindowWork />
+                <Tree user={user} />
+              </div>
+            );
+          }}
+      </ThemeContext.Consumer>
+    </>
   );
 };
 
