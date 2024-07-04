b90163a94ae79e9576589037d0381242a1f1e65e
Marta Prystatska <tradlisboa@gmail.com>
2024-07-04 01:03:11 +0100

    add change THEME for CounterPage

--- src/pages/CounterPage.jsx
+++ src/pages/CounterPage.jsx
@@ -1,11 +1,28 @@
 import React from "react";
+import cx from 'classnames';
+import styles from './CounterPage.module.scss'
+import { ThemeContext} from "../contexts";
 import CounterSection from "../components/CounterSection";
+import CONSTANTS from '../constants';
+const { THEME } = CONSTANTS; 
 
 const CounterPage = () => {
   return (
-    <div>
-      <CounterSection />
-    </div>
+    <>
+      <ThemeContext.Consumer>
+        {([theme]) => {
+          const classNames = cx(styles.header, {
+            [styles.light]: theme === THEME.LIGHT,
+            [styles.dark]: theme === THEME.DARK,
+          });
+          return (
+            <header className={classNames}
+            > <CounterSection />
+            </header>
+          );
+        }}
+      </ThemeContext.Consumer>
+    </>
   );
 };
 
