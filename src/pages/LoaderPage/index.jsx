11e4c0070cedbb6d25f56b1e77ead7f10c7a6751
Marta Prystatska <tradlisboa@gmail.com>
2024-07-04 02:11:31 +0100

    add Theme black/white for LoaderPage+create LoaderPage.module.scss

--- src/pages/LoaderPage/index.jsx
+++ src/pages/LoaderPage/index.jsx
@@ -1,20 +1,39 @@
 import React from "react";
-import { NavLink, Outlet} from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
+import cx from "classnames";
+import styles from "./LoaderPage.module.scss";
+import { ThemeContext } from "../../contexts";
+import CONSTANTS from "../../constants";
+const { THEME } = CONSTANTS;
+
 
 const LoaderPage = () => {
   return (
     <>
-      <ul>
-        <li>
-          <NavLink to="/load/events">events</NavLink>
-        </li>
-        <li>
-          <NavLink to="/load/phones">phones</NavLink>
-        </li>
-      </ul>
-      <Outlet />
+      <ThemeContext.Consumer>
+        {([theme]) => {
+          const classNames = cx(styles.header, {
+            [styles.light]: theme === THEME.LIGHT,
+            [styles.dark]: theme === THEME.DARK,
+          });
+          return (
+            <header className={classNames}>
+              <>
+                <ul>
+                  <li>
+                    <NavLink to="/load/events">events</NavLink>
+                  </li>
+                  <li>
+                    <NavLink to="/load/phones">phones</NavLink>
+                  </li>
+                </ul>
+                <Outlet />
+              </>
+            </header>
+          );
+        }}
+      </ThemeContext.Consumer>
     </>
   );
 };
-
 export default LoaderPage;
