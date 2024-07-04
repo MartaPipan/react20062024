d7fa441873554dda6f0315889e0425962dbd9725
Marta Prystatska <tradlisboa@gmail.com>
2024-07-04 01:12:38 +0100

    add style THEME in LoaderPage+create LoaderPage.module.scss

--- src/pages/UsersLoaderPage.jsx
+++ src/pages/UsersLoaderPage.jsx
@@ -1,12 +1,33 @@
 import React from 'react';
+import cx from 'classnames';
+import styles from './LoaderPage.module.scss'
+import { ThemeContext} from "../contexts";
 import UsersLoader from "../components/UsersLoader";
+import CONSTANTS from '../constants';
+const { THEME } = CONSTANTS; 
+
 
 const UsersLoaderPage = () => {
-    return (
-        <div>
-            <UsersLoader />
-        </div>
-    );
-}
+return (
+    <>
+      <ThemeContext.Consumer>
+        {([theme]) => {
+          const classNames = cx(styles.header, {
+            [styles.light]: theme === THEME.LIGHT,
+            [styles.dark]: theme === THEME.DARK,
+          });
+          return (
+            <header className={classNames}
+            > <UsersLoader />
+            </header>
+          );
+        }}
+      </ThemeContext.Consumer>
+    </>
+  );
+};
+
 
 export default UsersLoaderPage;
+
+
