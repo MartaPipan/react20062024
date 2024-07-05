7fa331d86f28c9baa6064976ced2126344bee59e
Marta Prystatska <tradlisboa@gmail.com>
2024-07-06 00:50:49 +0100

    Create folder utils;Validation SCHEMAS(Файл Utils містить кілька допоміжних функцій)

--- src/components/SignInForm/index.jsx
+++ src/components/SignInForm/index.jsx
@@ -1,28 +1,5 @@
 import React from "react";
-import * as Yup from 'yup';
-
-
-const SCHEMA_USER = Yup.object({
-  firstName: Yup.string()
-    .trim()
-    .required(campo de preenchimento obrigatorio)
-    .min(3,'no minimo 3 symbolos')
-    .max(20,'no maximo 20 symbolos')
-    .matches(/^[a-z]{3-20}$/,'apenas letras'),
-  lastName: Yup.string()
-    .trim()
-    .required()
-    .min(3)
-    .max(20)
-    .matches(/^[a-z]{3-20}$/),
-  email: Yup.string()
-    .trim()
-    .email()
-    .required(preenchimento obrigatorio com um endereço de email valido),
-  password: Yup.string().trim().matches(/[a-z0-9]{8-16}/),
-  age: Yup.number().min(18).max(100).integer().required(),
-  isMale: Yup.boolean(),
-})
+import { SCHEMA_USER } from '../../utils/validationSchemas';
 
 const SignInForm = () => {
   const user = {
---
+++ src/utils/validationSchemas.js
@@ -0,0 +1,58 @@
+import * as Yup from 'yup';
+
+
+export const NAME = Yup.string()
+  .trim()
+  .required('Campo de preenchimento obrigatório')
+  .min(3, 'No mínimo 3 símbolos')
+  .max(20, 'No máximo 20 símbolos')
+  .matches(/^[a-zA-Z]{3,20}$/, 'Apenas letras')
+
+
+export const LAST_NAME = Yup.string()
+  .trim()
+  .required('Campo de preenchimento obrigatório')
+  .min(3, 'No mínimo 3 símbolos')
+  .max(20, 'No máximo 20 símbolos')
+  .matches(/^[a-zA-Z]{3,20}$/, 'Apenas letras')
+
+
+export const AGE = Yup.number()
+  .min(18, 'Idade mínima é 18 anos')
+  .max(120, 'Idade máxima é 120 anos')
+  .integer('Idade deve ser um número inteiro')
+  .required('Campo de preenchimento obrigatório')
+
+
+export const IS_MALE = Yup.boolean()
+
+export const EMAIL = Yup.string()
+  .trim()
+  .matches(/^[a-zA-Z0-9]{8,16}$/, 'Senha deve ter entre 8 e 16 caracteres e conter apenas letras e números')
+  .required('Campo de preenchimento obrigatório')
+
+
+export const PASSWORD = Yup.string()
+  .trim()
+  .matches(/^[a-zA-Z0-9]{8,16}$/, 'Senha deve ter entre 8 e 16 caracteres e conter apenas letras e números')
+  .required('Campo de preenchimento obrigatório')
+
+
+export const SCHEMA_USER_REGISTER = Yup.object({
+  firstName: NAME,
+  lastName: LAST_NAME,
+  age: AGE,
+  isMale: IS_MALE,
+});
+
+
+export const SCHEMA_USER_SIGNIN = Yup.object({
+  email: EMAIL,
+  password: PASSWORD
+});
+
+
+export const SCHEMA_USER_FEEDBACK = Yup.object({
+  email: EMAIL,
+
+});
