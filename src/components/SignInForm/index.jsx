9a55ae0693cfb231bfce84c72646df166952af55
Marta Prystatska <tradlisboa@gmail.com>
2024-07-06 00:06:27 +0100

    Error.message; validate  SCHEMA (with promises)

--- src/components/SignInForm/index.jsx
+++ src/components/SignInForm/index.jsx
@@ -2,26 +2,44 @@ import React from "react";
 import * as Yup from 'yup';
 
 
-const SHEMA USER = Yup.object({
-  firstName: Yup.string().trim().required().min(3).max(20),
-  lastName: Yup.string().trim().required().min(3).max(20),
-  email: Yup.string().trim().email().required(),
+const SCHEMA_USER = Yup.object({
+  firstName: Yup.string()
+    .trim()
+    .required(campo de preenchimento obrigatorio)
+    .min(3,'no minimo 3 symbolos')
+    .max(20,'no maximo 20 symbolos')
+    .matches(/^[a-z]{3-20}$/,'apenas letras'),
+  lastName: Yup.string()
+    .trim()
+    .required()
+    .min(3)
+    .max(20)
+    .matches(/^[a-z]{3-20}$/),
+  email: Yup.string()
+    .trim()
+    .email()
+    .required(preenchimento obrigatorio com um endereço de email valido),
   password: Yup.string().trim().matches(/[a-z0-9]{8-16}/),
   age: Yup.number().min(18).max(100).integer().required(),
   isMale: Yup.boolean(),
 })
 
-
 const SignInForm = () => {
   const user = {
     firstName: 'Alan',
-  lastName:'Alan',
-  email: 'alan@gmail.com',
-  password: '123',
-  age: 32,
-  isMale: true,
+    lastName: 'Alan',
+    email: 'alan@gmail.com',
+    password: '123',
+    age: 32,
+    isMale: true,
   }
-  console.log(SCHEMA_USER.isValidSync(user));
+  console.log(
+    SCHEMA_USER.validate(user).then((info) => {
+      console.log(info);
+    }).catch((error) => {
+      console.log(error.message);
+    })
+  );
   return <></>
 };
 export default SignInForm;
