f2123b62551b62bd1ab48e906175146c01e32be2
Marta Prystatska <tradlisboa@gmail.com>
2024-07-07 23:07:48 +0100

    (Lesson 77-2.)InputValidate.jsx=>Field,form,.meta

--- src/components/form/InputValidate/index.jsx
+++ src/components/form/InputValidate/index.jsx
@@ -1,15 +1,46 @@
-import React from 'react';
-import { ErrorMessage, Field } from 'formik';
-import styles from './InputValidate.module.scss';
+import React from "react";
+import { ErrorMessage, Field } from "formik";
+import cx from "classnames";
+import styles from "./InputValidate.module.scss";
 
 const InputValidate = ({ name, type, placeholder }) => {
   return (
-      <label className={styles.label}>
-          <em>{name}</em>
-      <Field type={type} name={name} placeholder={placeholder} className={styles.inputField} />
+    <label className={styles.label}>
+      <em>{name}</em>
+
+      <Field name={name}>
+        {({ field, form, meta }) => {
+          console.log("field", field);
+          console.log("form", form);
+          console.log("meta", meta);
+          const classNames = cx({
+            [styles.invalid]: meta.touched && meta.error,
+          });
+          return (
+            <input
+              className={classNames}
+              type={type}
+              placeholder={placeholder}
+              {...field}
+            />
+          );
+        }}
+      </Field>
       <ErrorMessage name={name} component="span" className={styles.invalid} />
     </label>
   );
-}
+};
 
 export default InputValidate;
+
+//  <Field name={name}>
+//  {({ field, form, meta }) => {
+//          console.log('field',field);=====props necessarios para trabalho de formik);
+//          console.log('form', form);====FormikBag=informação para functionar(onChange,onCLICK,isValid+.....+ERROR)
+//          console.log('meta', meta);===value.toched,error
+//          return <input type={type} placeholder={placeholder}{...field} />
+//        }}
+//      </Field>
+
+
+//utils/ValidationSchema
