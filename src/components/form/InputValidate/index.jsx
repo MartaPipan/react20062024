a8da5334b51e312f693cb7e6c2a5166ba12d50ad
Marta Prystatska <tradlisboa@gmail.com>
2024-07-08 10:27:16 +0100

    add comments in InputValidate

--- src/components/form/InputValidate/index.jsx
+++ src/components/form/InputValidate/index.jsx
@@ -10,16 +10,15 @@ const CustomInput = ({ field, form: { touched, errors }, ...props }) => {
   });
   return <input {...props} {...field} className={classNames} />;
 };
-const InputValidate = ({ name, type, placeholder, props }) => {
+const InputValidate = ({ name, ...props }) => {
   return (
     <label className={styles.label}>
       <em>{name}</em>
 
       <Field
         name={name}
-        type={type}
-        placeholder={placeholder}
         component={CustomInput}
+        {...props}
       ></Field>
       <ErrorMessage name={name} component="span" className={styles.invalid} />
     </label>
@@ -38,3 +37,36 @@ export default InputValidate;
 //      </Field>
 
 //utils/ValidationSchema
+
+
+//
+//import React from "react";
+//import { ErrorMessage, Field } from "formik";
+//import cx from "classnames";
+//import styles from "./InputValidate.module.scss";
+
+//const CustomInput = ({ field, form: { touched, errors }, ...props }) => {
+//  const classNames = cx({
+//    [styles.invalid]: errors[field.name] && touched[field.name],
+//  });
+//  return <input className={classNames} {...field} {...props} />;
+//};
+
+//const InputValidate = ({ name, type, placeholder, ...props }) => {
+//  return (
+ //   <label className={styles.label}>
+ //     <em>{name}</em>
+  //    <Field
+//        name={name}
+//        type={type}
+//        placeholder={placeholder}
+ //       component={CustomInput}
+//        {...props}
+//      />
+//      <ErrorMessage name={name} component="span" className={styles.invalid} />
+//    </label>
+//  );
+//  };
+
+//export default InputValidate;
+
